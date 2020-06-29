a1f7f393876810ec2f2b2d4ddfc94d2768e4d1f0
Fridtjof Alestrøm <fridtjofalestrom@Fridtjofs-MacBook-Pro.local>
2020-06-29 14:18:49 +0200

    add clock

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Reddit from './Reddit';
 import Bikes from './Bikes';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <h1>Infoskjerm</h1>
+      <Clock />
       <Reddit />
       <Bikes />
+      
     </div>
   );
 }
