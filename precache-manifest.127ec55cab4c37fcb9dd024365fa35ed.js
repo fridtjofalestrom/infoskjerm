b15bdea7eb1465aadb48e99a821bdd564267e842
Fridtjof Alestrøm <fridtjofalestrom@Fridtjofs-MacBook-Pro.local>
2020-06-29 13:46:46 +0200

    Updates

---
+++ precache-manifest.127ec55cab4c37fcb9dd024365fa35ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee117cdf559c9592c5790a88b2787a93",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "408e85a05a9a51bbe3e9",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97cc9d08c3e0b47d6208",
+    "url": "/infoskjerm/static/js/2.4cf08f81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.4cf08f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408e85a05a9a51bbe3e9",
+    "url": "/infoskjerm/static/js/main.2c769564.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/infoskjerm/static/media/logo.5d5d9eef.svg"
+  }
+]);
